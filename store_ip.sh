2594fc50c4f55e70541d08aca073f2134c2d3759
mukeshdevelp <msmukeshkumarsharma95@gmail.com>
2025-11-21 21:57:05 +0530

    fixed store ip file

--- store_ip.sh
+++ store_ip.sh
@@ -2,7 +2,3 @@
 terraform output -json public_ip_of_bastion | jq -r '.' > private_ips.ini
 terraform output -json aws_private_instance_ip | jq -r '.[]' >> private_ips.ini
 
-# connect with private instaces
-ssh -i ./.ssh/sonarqube-key.pem   
--o 'ProxyCommand=ssh -i ./.ssh/sonarqube-key.pem -W %h:%p ubuntu@3.215.184.43'   ubuntu
-@10.0.4.159
